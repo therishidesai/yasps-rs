ebc627dace7594982a4e4228934902a3baabbb8b
Rishi Desai <desai.rishi1@gmail.com>
2021-12-24 21:04:23 -0800

    futex test with rust shared_memory crate

--- yasps-apps/futures-exp/main.rs
+++ yasps-apps/futures-exp/main.rs
@@ -1,18 +1,48 @@
-use linux_futex::{Futex, Private};
+use shared_memory::*;
 use std::future::Future;
 use std::pin::Pin;
-use std::sync::atomic::{AtomicBool, Ordering};
+use std::ptr;
+use std::sync::atomic::{AtomicBool, AtomicU8, Ordering};
 use std::sync::Arc;
 use std::task::{Context, Poll};
 use std::thread;
 use std::time::Duration;
 
-struct FutexTest {
-    f: Arc<Box<Futex<Private>>>,
+struct FutexTest<'a> {
+    ptr: Arc<&'a mut AtomicU8>,
     woke: Arc<AtomicBool>,
 }
 
-impl Future for FutexTest {
+impl FutexTest<'static> {
+    pub fn new(value: i32) -> Self {
+        let mut shmem = match ShmemConf::new().size(4096).os_id("RUST_TEST").create() {
+            Ok(m) => m,
+            Err(ShmemError::MappingIdExists) => ShmemConf::new().os_id("RUST_TEST").open().unwrap(),
+            Err(e) => {
+                println!("Unable to create or open shmem flink {} : {}", "test", e);
+                unsafe {
+                    libc::exit(1);
+                }
+            }
+        };
+
+        shmem.set_owner(false);
+        println!("SHMEM OS_ID: {}", shmem.get_os_id());
+
+        let raw_ptr = shmem.as_ptr();
+        unsafe {
+            *raw_ptr = 0;
+        }
+
+        let p = unsafe { Arc::new(&mut *(raw_ptr as *mut u8 as *mut AtomicU8)) };
+
+        Self {
+            ptr: p,
+            woke: Arc::new(AtomicBool::new(false)),
+        }
+    }
+}
+impl Future for FutexTest<'static> {
     type Output = &'static str;
 
     fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<&'static str> {
@@ -20,11 +50,17 @@ impl Future for FutexTest {
             return Poll::Ready("done");
         }
         let waker = cx.waker().clone();
-        let f = self.f.clone();
         let mut woke = self.woke.clone();
+        let mut p = self.ptr.clone();
         println!("poll");
         thread::spawn(move || {
-            (*f).wait(0);
+            let mut shmem = ShmemConf::new().os_id("RUST_TEST").open().unwrap();
+            shmem.set_owner(true);
+            let raw_ptr = shmem.as_ptr();
+            unsafe {
+                let null: *const i32 = ptr::null();
+                libc::syscall(libc::SYS_futex, raw_ptr, libc::FUTEX_WAIT, 0, null, null, 0);
+            }
             println!("Futex Bumped");
             (*woke).store(true, Ordering::Relaxed);
             waker.wake();
@@ -36,19 +72,27 @@ impl Future for FutexTest {
 
 #[tokio::main]
 async fn main() {
-    let f: Futex<Private> = Futex::new(0);
-
-    let bf = Arc::new(Box::new(f));
-    let bw = Arc::new(AtomicBool::new(false));
-    let future = FutexTest {
-        f: bf.clone(),
-        woke: bw,
-    };
-
+    let future = FutexTest::new(0);
     thread::spawn(move || {
+        let mut shmem = ShmemConf::new().os_id("RUST_TEST").open().unwrap();
+        shmem.set_owner(false);
         thread::sleep(Duration::from_millis(500));
+        let raw_ptr = shmem.as_ptr();
+        let p = unsafe { Arc::new(&mut *(raw_ptr as *mut u8 as *mut AtomicU8)) };
+        p.store(1, Ordering::Relaxed);
+        unsafe {
+            let null: *const i32 = ptr::null();
+            libc::syscall(
+                libc::SYS_futex,
+                raw_ptr,
+                libc::FUTEX_WAKE,
+                libc::INT_MAX,
+                null,
+                null,
+                0,
+            );
+        }
         println!("wakeup");
-        (*bf).wake(1);
     });
 
     let out = future.await;
